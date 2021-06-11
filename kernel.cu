7a52a95eb4d49d87f8b7f13436f98319dc537a26
Abbas Türkoğlu <abbasturkoglu@gmail.com>
2021-06-11 22:10:27 +0300

    Paralization completed.

--- kernel.cu
+++ kernel.cu
@@ -1,85 +1,196 @@
-﻿
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
 #include <stdio.h>
-
-#include <stdio.h>
-#include <cuda.h>
-
 // Do not alter the preprocessor directives
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "stb_image.h"
 #include "stb_image_write.h"
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <stdint.h>
+#include <driver_types.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+
 
 #define NUM_CHANNELS 1
 
+#define BLOCKSIZE 512
 
-// CPU baseline for finding minimum and maximum pixel values in an image
-void min_max_of_img_host(uint8_t* img, uint8_t* min, uint8_t* max, int width, int height) {
-    int max_tmp = 0;
-    int min_tmp = 255;
-    for (int n = 0; n < width * height; n++) {
-        max_tmp = (img[n] > max_tmp) ? img[n] : max_tmp;
-        min_tmp = (img[n] < min_tmp) ? img[n] : min_tmp;
-    }
-    *max = max_tmp;
-    *min = min_tmp;
+#define MinVal(x, y) (((x) < (y)) ? (x) : (y))
+#define MaxVal(x, y) (((x) > (y)) ? (x) : (y))
 
+
+
+__global__ void minKernel(uint8_t *image, int size, uint8_t *odata)
+{
+	extern __shared__ volatile uint8_t sdata[];
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*(BLOCKSIZE)+tid;
+	unsigned int gridSize = BLOCKSIZE * gridDim.x;
+
+	uint8_t val = image[i];
+	i += gridSize;
+	while (i < size) {
+		val = MinVal(image[i], val);
+		i += gridSize;
+	}
+	sdata[tid] = val;
+	__syncthreads();
+
+	// Parallel reduction v5
+	for (int i = (tid + 32); ((tid < 32) && (i < BLOCKSIZE)); i += 32)
+		sdata[tid] = MinVal(sdata[tid], sdata[i]);
+
+	if (tid < 16) sdata[tid] = MinVal(sdata[tid], sdata[tid + 16]);
+	if (tid < 8)  sdata[tid] = MinVal(sdata[tid], sdata[tid + 8]);
+	if (tid < 4)  sdata[tid] = MinVal(sdata[tid], sdata[tid + 4]);
+	if (tid < 2)  sdata[tid] = MinVal(sdata[tid], sdata[tid + 2]);
+	if (tid == 0) odata[blockIdx.x] = MinVal(sdata[tid], sdata[tid + 1]);
 }
 
-// CPU baseline for subtracting a value from all pixels in an image
-void sub_host(uint8_t* img, uint8_t sub_value, int width, int height) {
-    for (int n = 0; n < width * height; n++) {
-        img[n] -= sub_value;
-    }
+
+
+__global__ void maxKernel(uint8_t *image, int size, uint8_t *odata)
+{
+	extern __shared__ volatile uint8_t sdata[];
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*(BLOCKSIZE)+tid;
+	unsigned int gridSize = BLOCKSIZE * gridDim.x;
+
+	uint8_t val = image[i];
+	i += gridSize;
+	while (i < size) {
+		val = MaxVal(image[i], val);
+		i += gridSize;
+	}
+	sdata[tid] = val;
+	__syncthreads();
+
+
+	// Parallel reduction v5
+
+	for (int i = (tid + 32); ((tid < 32) && (i < BLOCKSIZE)); i += 32)
+		sdata[tid] = MaxVal(sdata[tid], sdata[i]);
+
+	if (tid < 16) sdata[tid] = MaxVal(sdata[tid], sdata[tid + 16]);
+	if (tid < 8)  sdata[tid] = MaxVal(sdata[tid], sdata[tid + 8]);
+	if (tid < 4)  sdata[tid] = MaxVal(sdata[tid], sdata[tid + 4]);
+	if (tid < 2)  sdata[tid] = MaxVal(sdata[tid], sdata[tid + 2]);
+	if (tid == 0) odata[blockIdx.x] = MaxVal(sdata[tid], sdata[tid + 1]);
 }
 
-
-// CPU baseline for scaling pixel values in an image avoiding finite precision
-// integer arithmetic given "power" and "constant" values.
-void scale_host(uint8_t* img, float constant, int width, int height) {
-    for (int n = 0; n < width * height; n++) {
-        img[n] = img[n] * constant; //note the implicit type conversion
-    }
+__global__ void SubKernel(uint8_t *image, uint8_t min_value)
+{
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	image[i] = image[i] - min_value;
 }
 
+__global__ void ScaleKernel(uint8_t *image, float scale_constant)
+{
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	image[i] = image[i] * scale_constant;
+}
 
 int main() {
-    int width; //image width
-    int height; //image height
-    int bpp;  //bytes per pixel if the image was RGB (not used)
 
-    uint8_t min_host, max_host;
+	int width; //image width
+	int height; //image height
+	int bpp;  //bytes per pixel if the image was RGB (not used)
 
-    // Load a grayscale bmp image to an unsigned integer array with its height and weight.
-    //  (uint8_t is an alias for "unsigned char")
-    uint8_t* image = stbi_load("./samples/1280x843.bmp", &width, &height, &bpp, NUM_CHANNELS);
 
-    // Print for sanity check
-    printf("Bytes per pixel: %d \n", bpp / 3); //Image is grayscale, so bpp / 3;
-    printf("Height: %d \n", height);
-    printf("Width: %d \n", width);
 
-    // Calculate the value of minimum and maximum pixels (to be replaced with GPU kernel)
-    min_max_of_img_host(image, &min_host, &max_host, width, height);
+	// Load a grayscale bmp image to an unsigned integer array with its height and weight.
+	//  (uint8_t is an alias for "unsigned char")
+	uint8_t* image = stbi_load("./samples/1280x843.bmp", &width, &height, &bpp, NUM_CHANNELS);
+	size_t image_size = width * height * sizeof(uint8_t);
 
-    float scale_constant = 255.0f / (max_host - min_host);
+	// Print for sanity check
+	printf("Bytes per pixel: %d \n", bpp / 3); //Image is grayscale, so bpp / 3;
+	printf("Height: %d \n", height);
+	printf("Width: %d \n", width);
 
-    // Subtract minimum pixel value from all pixels (to be replaced with GPU kernel)
-    sub_host(image, min_host, width, height);
 
-    // Scale pixel values between 0 and 255 (to be replaced with GPU kernel)
-    scale_host(image, scale_constant, width, height);
+	//Start Counter
+	cudaEvent_t start, stop;
+	float elapsed_time_ms;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
 
-    // Write image array into a bmp file
-    stbi_write_bmp("./out_img.bmp", width, height, 1, image);
+	//Device variables
+	uint8_t* min_d;
+	uint8_t* max_d;
+	uint8_t* image_d;
 
-    // Deallocate memory
-    stbi_image_free(image);
+	//Kernel execution sizes
+	unsigned int GRIDSIZE = width / (BLOCKSIZE * 1.0);
+	unsigned int GRIDSIZE2 = ceil((width * height) / (BLOCKSIZE * 1.0));
 
-    return 0;
+	//Allocate memory for host varables
+	uint8_t* min_host = (uint8_t*)malloc(GRIDSIZE * sizeof(uint8_t));
+	uint8_t* max_host = (uint8_t*)malloc(GRIDSIZE * sizeof(uint8_t));
+
+	//CUDA allocate memory
+	cudaMalloc((void**)&min_d, sizeof(uint8_t) * GRIDSIZE);
+	cudaMalloc((void**)&max_d, sizeof(uint8_t) * GRIDSIZE);
+	cudaMalloc((void**)&image_d, image_size);
+
+	//Copy image values to device
+	cudaMemcpy(image_d, image, image_size, cudaMemcpyHostToDevice);
+
+
+
+	//kernels to find minimum and maximum values
+	minKernel << <GRIDSIZE, BLOCKSIZE, sizeof(uint8_t)*BLOCKSIZE >> > (image_d, width* height, min_d);
+	maxKernel << <GRIDSIZE, BLOCKSIZE, sizeof(uint8_t)*BLOCKSIZE >> > (image_d, width*height, max_d);
+
+
+	//Get min and max values
+	cudaMemcpy(min_host, min_d, sizeof(uint8_t) * GRIDSIZE, cudaMemcpyDeviceToHost);
+	cudaMemcpy(max_host, max_d, sizeof(uint8_t) * GRIDSIZE, cudaMemcpyDeviceToHost);
+
+
+	//Subtraction Kernel
+	SubKernel << <GRIDSIZE2, BLOCKSIZE >> > (image_d, min_host[0]);
+
+
+	float scale_constant = 255.0f / (max_host[0] - min_host[0]);
+
+	//Scale Kernel
+	ScaleKernel << <GRIDSIZE2, BLOCKSIZE >> > (image_d, scale_constant);
+
+	//Copy image from device to host
+	cudaMemcpy(image, image_d, image_size, cudaMemcpyDeviceToHost);
+
+
+	//Stop timing
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsed_time_ms, start, stop);
+	printf("\nTime to calculate results(GPU Time): %f ms.\n\n", elapsed_time_ms);
+
+	printf("Minimum Pixel Value: %d\n", min_host[0]);
+	printf("Maximum Pixel Value: %d\n", max_host[0]);
+
+	// Write image array into a bmp file
+	stbi_write_bmp("./samples/out_img.bmp", width, height, 1, image);
+	printf("\nEnchanced image successfully saved.\n");
+
+
+
+
+	//free memory
+	free(min_host);
+	free(max_host);
+	cudaFree(min_d);
+	cudaFree(max_d);
+	cudaFree(image_d);
+
+	return 0;
 }
